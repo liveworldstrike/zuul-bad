4fd0322a25af416b6b38fb88ea59c1dd681e0f0c
liveworldstrike <lazodellamas12@gmail.com>
2016-05-18 12:26:18 +0200

    0127:terminado jar de proyecto

---
+++ WorldOfZuul.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    // instance variables - replace the example below with your own
+    private static Game NEWGAME = new Game();
+    /**
+     * 
+     */
+    public static void main(String[] args)
+    {
+        NEWGAME.play();
+        
+    }
+
+    
+}
